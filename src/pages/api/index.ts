31bc0de67316d836cc3b2a5db6463e3975589c3f
Hyuksang-Oh <iuesver@gmail.com>
2023-03-11 19:01:10 +0900

    feat : open ai api 연동

---
+++ src/pages/api/index.ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse, NextApiHandler } from "next";
+import { Configuration, OpenAIApi } from "openai";
+
+const API_KEY = process.env.OPENAI_API_KEY;
+
+const configuration = new Configuration({
+  apiKey: API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const handler: NextApiHandler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const prompt = req.body.prompt;
+
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      max_tokens: 2048,
+      temperature: 0,
+    });
+    res.status(200).json({ response: response.data.choices[0].text?.trim() });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Something's wrong." });
+  }
+};
+
+export default handler;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Head from "next/head";
 import { GetServerSideProps } from "next";
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 interface Data {
   title: string;
@@ -12,9 +12,24 @@ interface Data {
 }
 
 export default function Home({ data }: { data: Data[] }) {
+  const [prompt, setPrompt] = useState<string>("");
+  const [response, setResponse] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await axios.post("/api", { prompt: prompt });
+      setResponse(data.response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
-    console.log(data);
-  }, []);
+    console.log(response);
+  }, [response]);
+
   return (
     <>
       <Head>
@@ -27,21 +42,34 @@ export default function Home({ data }: { data: Data[] }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div>hi</div>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Enter a prompt:
+              <input
+                type="text"
+                value={prompt}
+                onChange={(e) => setPrompt(e.target.value)}
+              />
+            </label>
+            <button type="submit">Submit</button>
+          </form>
+          {response && <p>{response}</p>}
+        </div>
       </main>
     </>
   );
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const response = await axios.get(`${process.env.API_URL}`, {
+  const response = await axios.get(`${process.env.NAVER_API_URL}`, {
     headers: {
-      "X-Naver-Client-Id": `${process.env.API_ID}`,
-      "X-Naver-Client-Secret": `${process.env.API_SECRET}`,
+      "X-Naver-Client-Id": `${process.env.NAVER_API_ID}`,
+      "X-Naver-Client-Secret": `${process.env.NAVER_API_SECRET}`,
     },
     params: {
       query: "오늘의 뉴스",
-      display: 10,
+      display: 100,
       sort: "sim",
       start: 1,
     },
